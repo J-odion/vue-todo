eb56aed24a1af2c8c3d49e5a6605feacf2482c47
Jafaru Emmanuel <emmyjaff22@gmail.com>
2021-08-26 03:36:48 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="about pa-6">
-    <h1>About Veutify Applications</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores placeat perspiciatis perferendis! Minus maiores, tempore reprehenderit reiciendis soluta odio, accusamus obcaecati placeat ducimus expedita eum quidem saepe doloribus quod optio.</p>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores placeat perspiciatis perferendis! Minus maiores, tempore reprehenderit reiciendis soluta odio, accusamus obcaecati placeat ducimus expedita eum quidem saepe doloribus quod optio.</p>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores placeat perspiciatis perferendis! Minus maiores, tempore reprehenderit reiciendis soluta odio, accusamus obcaecati placeat ducimus expedita eum quidem saepe doloribus quod optio.</p>
+    <h1>About My Veutify Todo Applications</h1>
+    <p>Hi, I am Emmanuel Jafaru. I like to go by Emmyjaff. I am a web developer. keen at learning new technologies for web development. </p>
+    <p>I am a student of Obafemi Awolowo University, ile-ife. I study Animal Sciences, faculty of Agriculture. I know that expression, a farmer in the tech space again? Well, you got it</p>
+    <br />
+    <p>This application is a basic todo application that sits on Vue and uses vuetify. For version 1, it has just the delete function working. I would keep improving this piece of application as time and inspiration leads</p>
   </div>
 </template>
