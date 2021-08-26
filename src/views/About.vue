53833dbedcfddcede3465132a75e03b06250d5ac
Jafaru Emmanuel <emmyjaff22@gmail.com>
2021-08-26 02:55:35 +0100

    Vue js todo list using Vuetify library for styling
    Vue js todo list using Vuetify library for styling

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Veutify Applications</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores placeat perspiciatis perferendis! Minus maiores, tempore reprehenderit reiciendis soluta odio, accusamus obcaecati placeat ducimus expedita eum quidem saepe doloribus quod optio.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores placeat perspiciatis perferendis! Minus maiores, tempore reprehenderit reiciendis soluta odio, accusamus obcaecati placeat ducimus expedita eum quidem saepe doloribus quod optio.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores placeat perspiciatis perferendis! Minus maiores, tempore reprehenderit reiciendis soluta odio, accusamus obcaecati placeat ducimus expedita eum quidem saepe doloribus quod optio.</p>
+  </div>
+</template>
